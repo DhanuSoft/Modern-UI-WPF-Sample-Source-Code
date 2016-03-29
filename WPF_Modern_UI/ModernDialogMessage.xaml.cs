83b582551c0bfa3c66267583496d2acfbba746ab
P.H.P.D.Gamlath <prasaddhananjaya92@gmail.com>
2016-03-29 08:39:58 +0530

    Added files via upload

---
+++ WPF_Modern_UI/ModernDialogMessage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using FirstFloor.ModernUI.Windows.Controls;
+
+namespace WPF_Modern_UI
+{
+    /// <summary>
+    /// Interaction logic for ModernDialogMessage.xaml
+    /// </summary>
+    public partial class ModernDialogMessage : ModernDialog
+    {
+        public ModernDialogMessage()
+        {
+            InitializeComponent();
+        }
+    }
+}
